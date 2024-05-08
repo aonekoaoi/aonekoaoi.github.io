87a1e49326cac7411d61a45487138a256b00f5b0
aonekoaoi <132233257+aonekoaoi@users.noreply.github.com>
2024-05-08 22:12:00 +0900

    Add: File(s)
    - File(s)
        - css
            - media.css
            - style.css
        - img
            - aonekoaoi.png
            - battle.net.png
            - bluesky.png
            - electronic-arts.png
            - epci-games.png
            - github.png
            - hugging-face.png
            - instagram.png
            - misskey.png
            - pixiv.png
            - playstation.png
            - railway.png
            - replit.png
            - riot-games.png
            - steam.png
            - threads.png
            - ubisoft.png
            - x.png
            - xbox.png
        - js
            - script.js
        - 404.html
        - index.html
        - LICENSE_ja.txt
        - README.md
    
    Signed-off-by: aonekoaoi <132233257+aonekoaoi@users.noreply.github.com>

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// export script.js import aonekoaoi.github.io
+
+function PlayStation() {
+  // PlayStation alert message id
+  alert("PlayStation@aoneko401");
+}
+
+function XBOX() {
+  // XBOX alert message id
+  alert("XBOX@aoneko401");
+}
+
+function Electronic_Arts() {
+  // Electronic Arts alert message id
+  alert("Electronic Arts@aoneko401");
+}
+
+function EPIC_GAMES() {
+  // EPIC GAMES alert message id
+  alert("EPIC GAMES@aoneko401");
+}
+
+function Ubisoft() {
+  // Ubisoft alert message id
+  alert("Ubisoft@aoneko401");
+}
+
+function Battle_net() {
+  // Battle.net alert message id
+  alert("Battle.net@aoneko401#3983");
+}
+
+function RIOT_GAMES() {
+  // RIOT GAMES alert message id
+  alert("RIOT GAMES@aoneko401#3983");
+}
